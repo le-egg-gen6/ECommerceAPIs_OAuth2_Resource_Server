cedb7bc3b6984f10c6b493c1ec1126479a6e5f7d
Le Nguyen Ngoc <lebkp04@gmail.com>
2024-04-27 06:08:48 +0700

    some config

---
+++ src/main/java/com/myproject/resource_server/listener/PasswordForgotListener.java
@@ -0,0 +1,8 @@
+package com.myproject.resource_server.listener;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordForgotListener implements ApplicationListener<OnRe> {
+}
