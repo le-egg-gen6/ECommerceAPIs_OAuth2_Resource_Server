1fb74887a537815164920de0528323fa3008acaf
Le Nguyen Ngoc <lebkp04@gmail.com>
2024-04-27 19:54:16 +0700

    add DAO Layer

---
+++ src/main/java/com/myproject/resource_server/repository/OrderRepository.java
@@ -0,0 +1,19 @@
+package com.myproject.resource_server.repository;
+
+import com.myproject.resource_server.model.Order;
+import com.myproject.resource_server.model.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
+
+    List<Order> findAllByUserOrderByDateDesc(User user, Pageable pageable);
+
+    Optional<Integer> countAllByUser(User user);
+
+}
---
+++ src/main/java/com/myproject/resource_server/repository/ProductVariantRepository.java
@@ -0,0 +1,15 @@
+package com.myproject.resource_server.repository;
+
+import com.myproject.resource_server.model.ProductVariant;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductVariantRepository extends PagingAndSortingRepository<ProductVariant, Long>,
+        JpaSpecificationExecutor<ProductVariant> {
+    List<ProductVariant> findTop8ByOrderBySellCountDesc();
+
+}
