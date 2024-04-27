7901d00f90f079250cf0904c950f1d57e5c34fc2
Le Nguyen Ngoc <lebkp04@gmail.com>
2024-04-28 00:24:41 +0700

    add converter from model to response

--- src/main/java/com/myproject/resource_server/model/BaseModel.java
+++ src/main/java/com/myproject/resource_server/model/BaseModel.java
@@ -1,8 +1,14 @@
 package com.myproject.resource_server.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @MappedSuperclass
+@Getter
+@Setter
+@ToString
 public abstract class BaseModel {
 
     @Id
---
+++ src/main/java/com/myproject/resource_server/service/UserService.java
@@ -0,0 +1,4 @@
+package com.myproject.resource_server.service;
+
+public interface UserService {
+}
