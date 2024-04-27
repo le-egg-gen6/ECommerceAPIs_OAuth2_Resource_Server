13cc40cb44da8c8db6c5e56364c3615a8147e2df
Le Nguyen Ngoc <lebkp04@gmail.com>
2024-04-27 22:50:08 +0700

    add some annotation validator

---
+++ src/main/java/com/myproject/resource_server/validator/PasswordMatchesValidator.java
@@ -0,0 +1,28 @@
+package com.myproject.resource_server.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext context) {
+        if (obj instanceof RegisterUserRequest) {
+            RegisterUserRequest registerUserRequest = (RegisterUserRequest) obj;
+            return registerUserRequest.getPassword().equals(registerUserRequest.getPasswordRepeat());
+        } else if (obj instanceof PasswordResetRequest) {
+            PasswordResetRequest passwordResetRequest = (PasswordResetRequest) obj;
+            return passwordResetRequest.getNewPassword().equals(passwordResetRequest.getNewPasswordConfirm());
+        } else if (obj instanceof PasswordForgotValidateRequest) {
+            PasswordForgotValidateRequest passwordForgotValidateRequest = (PasswordForgotValidateRequest) obj;
+            return passwordForgotValidateRequest.getNewPassword().equals(passwordForgotValidateRequest.getNewPasswordConfirm());
+        }
+
+        return false;
+
+    }
+}
